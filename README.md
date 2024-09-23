fb20004b1dd1bcf22b37302f0d4e0e4398e5640e
globalGLEX <priitkru@gmail.com>
2024-09-23 13:58:01 +0300

    fix folder issues

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + TypeScript + Vite
+
+Current plan - detect aircraft that come close and notify the user on browser.
---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+       
+        
+      </div>
+      <h1>aircraft5</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
